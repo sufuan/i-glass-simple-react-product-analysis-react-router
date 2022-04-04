a22ca5cc2cfbdb81f6d8cbfdfd490097bf634395
sufuan <abu49539@gmail.com>
2022-04-04 19:49:40 +0600

    finished

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className='sticky top-0 bg-white px-12 py-2 flex justify-center md:justify-between'>
-                <div className='hidden md:block' >
-                   <Link to='/' className='font-bold text-2xl text-gray-600'>i-glass</Link>
-                </div>
-                <div className='flex gap-4 text-xl '>
-                    <Link to='/'>Home</Link>
-                    <Link to='/review'>Review</Link>
-                    <Link to='/dashboard'>Dashboard</Link>
-                    <Link to='/blog'>Blog</Link>
-                    <Link to='/about'>About</Link>
+            <div className='hidden md:block' >
+                <Link to='/' className='font-bold text-2xl text-gray-600'>i-glass</Link>
+            </div>
+            <div className='flex gap-4 text-xl '>
+                <NavLink to='/' className={({ isActive }) => isActive ? 'text-emerald-400' : 'black'}>Home</NavLink>
 
-                </div>
+                <NavLink to='/review' className={({ isActive }) => isActive ? 'text-emerald-400' : 'black'}>Review</NavLink>
+
+                <NavLink to='/dashboard' className={({ isActive }) => isActive ? 'text-emerald-400' : 'black'}>Dashboard</NavLink>
+                
+                <NavLink to='/blog' className={({ isActive }) => isActive ? 'text-emerald-400' : 'black'}>Blog</NavLink>
+
+                <NavLink to='/about' className={({ isActive }) => isActive ? 'text-emerald-400' : 'black'}>About</NavLink>
+
+            </div>
         </div>
     );
 };
