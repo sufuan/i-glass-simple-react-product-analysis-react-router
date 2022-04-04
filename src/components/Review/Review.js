aeec5690f5ec5f9d2d50209a7ee787048ba783cb
sufuan <abu49539@gmail.com>
2022-04-04 11:04:58 +0600

    review loaded

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -11,18 +11,26 @@ const Review = () => {
         .then(data=>setReviews(data))
     },[])
     return (
-        <div>
-              <h1>  this is revirew</h1>
+
+        <div className="w-90vh px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2'">
+    <div className='grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center'>
+           
               
-              {
-                  reviews.map(review=><ReviewDetails 
-                    review={review}
-                    key={review.id}
-                    
-                    ></ReviewDetails>)
-              }
+           {
+               reviews.map(review=><ReviewDetails 
+                 review={review}
+                 key={review.id}
+                 
+                 ></ReviewDetails>)
+           }
+
+     </div>
 
         </div>
+
+
+
+    
     );
 };
 
--- src/components/ReviewDetails/ReviewDetails.js
+++ src/components/ReviewDetails/ReviewDetails.js
@@ -6,7 +6,20 @@ const ReviewDetails = (props) => {
 
     return (
         <div>
-                this is details{name}
+                  <div className='shadow-lg rounded-2xl bg-white h-52 p-4'>
+                <div className="flex gap-4 ">
+                    <div className="flex-shrink-0 ">
+                        <img className='h-16 w-16 mx-auto rounded-full object-cover' src={picture} alt="profile" />
+                    </div>
+                    <div className="flex flex-col justify-end">
+                    <p className='text-gray-400 text-xs'>{review}</p>
+
+                        <p className='text-gray-600 text-medium'>{name}</p>
+                    </div>
+                </div>
+         
+
+        </div>
         </div>
     );
 };
